5b1d163799bf0a86bad8823d4f7afc859b3fbcbd
YaroslavLe <i.lieshukov@gmail.com>
2023-09-10 00:34:20 +0300

    .

--- ALU.sv
+++ ALU.sv
@@ -1,34 +1,35 @@
 module ALU #(
 	parameter A_WIDTH = 32)
 	
-(	input logic [2:0] ALUControl,
-	input logic [A_WIDTH-1:0] A, B,
+(	input logic [3:0] ALUControl,
+	input logic signed [A_WIDTH-1:0] A, B,
 	output logic [A_WIDTH-1:0] Result, 
 	output logic [3:0] Flags);
 	
 	always_comb begin
-	case (ALUControl)
-	3'b000: Result = A + B; //add
-	3'b001: Result = A - B;//sub
-	3'b010: Result = A && B; //AND
-	3'b011: Result = A || B;//OR
-	3'b101: Result = A < B; //SLT
-	defalut: Result = 3'bxxx;
-	endcase
-	
 	logic Negative, Zero, Carry, Overflow;
+	case (ALUControl)
+		4'b0000: Result = A + B; //ADD
+		4'b0001: Result = A - B;//SUB
+		4'b0010: Result = A && B; //AND
+		4'b0011: Result = A || B;//OR
+		4'b0101: Result = unsigned(A) < unsigned(B); //SLT
+		4'b0110: Result = A << B; //SLL
+		4'b0111: Result = A >> B; //SRL
+		4'b1000: Result = A ^ B; //XOR
+		4'b1001: Result = A < B; //SLTU
+		default: Result = 4'bxxx;
+	endcase
 	
 	Zero = ~&Result;
 	Negative = Result[A_WIDTH-1];
-	if (((A>0 && A+B<0) || (A<0 && A+B>0)) && (A * B >= 0)) Overflow = 1'b1;
+	if ((A>0 && B>0 && A+B<0) || (A<0 && B<0 && A+B>0)) Overflow = 1'b1;
 	else Overflow = 0;
-	//I dont know which option is better (probably without multiplication)
 	//((A>0 && A+B<0) || (A<0 && A+B>0)) && (A * B >= 0)
 	//(A>0 && B>0 && A+B<0) || (A<0 && B<0 && A+B>0)
-	if (Result > 0xFFFFFFFF) Carry = 1'b1;
-	else Carry = 0;
-	Flag = {Negative, Zero, Carry, Overflow};
-	
+	if (Result>32'hFFFFFFFF) Carry = 1'b1;
+	else Carry = 1'b0;
+	Flags = {Negative, Zero, Carry, Overflow};
 	end 
  
 endmodule
--- InstructionsMemory.sv
+++ InstructionsMemory.sv
@@ -2,13 +2,15 @@ module InstructionsMemory#(
 	parameter ADDR_WIDTH = 5,
 	parameter DATA_WIDTH = 32)
 (
+	input logic clk,
 	input logic ADDR,
+	input logic WD,
 	output logic RD);
 	
 logic [DATA_WIDTH-1:0] memory [2**ADDR_WIDTH];
 always_ff @ (posedge clk) begin
 	 
-		memory[ADDR] <= WD3;
+		memory[ADDR] <= WD;
 end
 assign RD = memory[ADDR];
-endmodule;
+endmodule
